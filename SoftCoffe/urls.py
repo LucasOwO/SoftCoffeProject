e484514e607d7af5638366af63e616af8d248e4a
Lucario_OwO <lucasalfredosan@gmail.com>
2024-04-10 18:37:17 -0400

    statics

--- SoftCoffe/urls.py
+++ SoftCoffe/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('PaginaWeb.urls')),
-    
+    path('', include('PaginaWeb.urls'))
     #path('admin/', admin.site.urls),
     
 ]
