04783d26c8cdde02cbaad21ea3d024f4bdebcb59
Lucario_OwO <lucasalfredosan@gmail.com>
2024-04-23 21:27:44 -0400

    Relaciones

--- PaginaWeb/admin.py
+++ PaginaWeb/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import usuario, tipo_usuario, reserva, pedido, producto
+from .models import usuario, tipo_usuario, reserva, pedido, producto, pedido_producto, pedido_usuario, usuario_reserva
 
 # Register your models here.
 
@@ -8,3 +8,6 @@ admin.site.register(tipo_usuario)
 admin.site.register(reserva)
 admin.site.register(pedido)
 admin.site.register(producto)
+admin.site.register(pedido_usuario)
+admin.site.register(pedido_producto)
+admin.site.register(usuario_reserva)
--- PaginaWeb/models.py
+++ PaginaWeb/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class tipo_usuario(models.Model):
+    id_tipo = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=15)
+    
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.nombre)
+
 class usuario(models.Model):
     id_usuario = models.PositiveIntegerField(primary_key=True)
     nombres = models.CharField(max_length=30)
@@ -10,35 +18,12 @@ class usuario(models.Model):
     fec_nac = models.DateField()
     correo = models.CharField(max_length=40)
     contra = models.CharField(max_length=40)
+    tipo_usuario = models.ForeignKey(tipo_usuario, null=True, on_delete=models.CASCADE)
     
     def __str__(self):
         texto = "{0} {1}"
         return texto.format(self.nombres, self.apellidos)
-
-class tipo_usuario(models.Model):
-    id_tipo = models.PositiveIntegerField(primary_key=True)
-    nombre = models.CharField(max_length=15)
     
-    def __str__(self):
-        texto = "{0}"
-        return texto.format(self.nombre)
-    
-class reserva(models.Model):
-    id_reserva = models.PositiveIntegerField(primary_key=True)
-    fec_res = models.DateField();
-    
-    def __str__(self):
-        texto = "Reserva nro {0}"
-        return texto.format(self.id_reserva)
-    
-class pedido(models.Model):
-    cod_pedido = models.PositiveIntegerField(primary_key=True)
-    fec_ped = models.DateField()
-    
-    def __str__(self):
-        texto = "Pedido {0}"
-        return texto.format(self.cod_pedido)
-
 class producto(models.Model):
     id_prod = models.PositiveIntegerField(primary_key=True)
     nombre = models.CharField(max_length=25)
@@ -49,3 +34,39 @@ class producto(models.Model):
     def __str__(self):
         texto = "{0}"
         return texto.format(self.nombre)
+    
+class pedido(models.Model):
+    cod_pedido = models.PositiveIntegerField(primary_key=True)
+    fec_ped = models.DateField()
+    
+    def __str__(self):
+        texto = "Pedido {0}"
+        return texto.format(self.cod_pedido)
+
+class medio_pago(models.Model):
+    id_pago = models.PositiveIntegerField(primary_key=True)
+    nombre = models.CharField(max_length=10)
+    
+    
+class pedido_producto(models.Model):
+    cod_pedido = models.ForeignKey(pedido, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+
+class pedido_usuario(models.Model):
+    cod_pedido = models.ForeignKey(pedido, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    id_pago = models.ForeignKey(medio_pago, on_delete=models.CASCADE)
+    
+
+class reserva(models.Model):
+    id_reserva = models.PositiveIntegerField(primary_key=True)
+    fec_res = models.DateField();
+    
+    def __str__(self):
+        texto = "Reserva nro {0}"
+        return texto.format(self.id_reserva)
+   
+class usuario_reserva(models.Model):
+    id_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    id_reserva = models.ForeignKey(reserva, on_delete=models.CASCADE)
